3fe9ef163a5504b7f8862dc2802e54107a0fdfb9
Lukas Winkler <git@lw1.at>
2020-12-27 13:47:03 +0100

    many improvements to parameters

--- water_sim.py
+++ water_sim.py
@@ -2,13 +2,27 @@ import time
 from math import radians
 
 import numpy as np
-from rebound import Simulation, Particle, Collision, reb_simulation_integrator_mercurius, NoParticles
+from numpy import linalg
+from rebound import Simulation, Particle, reb_simulation_integrator_mercurius, NoParticles, Collision, Orbit, Escape
 from scipy.constants import astronomical_unit
 
 from extradata import ExtraData, ParticleData
-from merge import merge_particles
+from merge import merge_particles, handle_escape
 from radius_utils import radius
 from utils import unique_hash, filename_from_argv
+
+
+def innermost_period(sim: Simulation) -> float:
+    minp = None
+    mina = float("inf")
+
+    for p in sim.particles[1:]:
+        if p.a < mina:
+            mina = p.a
+            minp = p
+    orb: Orbit = minp.orbit
+    return orb.P
+
 
 start = time.perf_counter()
 
@@ -17,27 +31,32 @@ sim = Simulation()
 fn = filename_from_argv()
 
 sim.units = ('yr', 'AU', 'kg')
-sim.boundary = "open"
-boxsize = 100
-sim.configure_box(boxsize)
+# sim.boundary = "open"
+# boxsize = 100
+# sim.configure_box(boxsize)
+sim.exit_max_distance = 30
 sim.integrator = "mercurius"
+# sim.collision = 'line'
+# sim.ri_whfast.coordinates = "democraticheliocentric"
 sim.dt = 1e-2
-# sim.ri_whfast.safe_mode = 0
+sim.ri_ias15.min_dt = 0.0001 / 365
+sim.ri_whfast.safe_mode = 0
 sim.collision = "direct"
-# sim.ri_mercurius.hillfac = 3.
+sim.ri_mercurius.hillfac = 3.
+sim.ri_whfast.corrector = 11
 # sim.collision_resolve = "merge"
 
 extradata = ExtraData()
 
 i = 0
-with open("initcon/conditions.input") as f:
+with open("initcon/conditions_many.input") as f:
     for line in f:
         if line.startswith("#") or line.startswith("ERROR") or line == "\n":
             continue
         columns = list(map(float, line.split()))
         hash = unique_hash()
         if len(columns) > 7:
-            print(columns[7:])
+            # print(columns[7:])
             cmf, mmf, wmf = columns[7:]
             assert cmf + mmf + wmf == 1
             extradata.pdata[hash.value] = ParticleData(water_mass_fraction=wmf)
@@ -46,31 +65,34 @@ with open("initcon/conditions.input") as f:
         if columns[1] == 0:  # that should not be needed, but nevertheless is
             part = Particle(m=columns[0], hash=hash)
         else:
-            part = Particle(m=columns[0], a=columns[1], e=columns[2],
-                            inc=radians(columns[3]), omega=radians(columns[4]),
-                            Omega=radians(columns[5]), M=radians(columns[6]),
-                            simulation=sim,
-                            hash=hash,
-                            r=radius(columns[0], wmf) / astronomical_unit
-                            )
+            part = Particle(
+                m=columns[0], a=columns[1], e=columns[2],
+                inc=radians(columns[3]), omega=columns[4],
+                Omega=columns[5], M=columns[6],
+                simulation=sim,
+                hash=hash,
+                r=radius(columns[0], wmf) / astronomical_unit
+            )
         sim.add(part)
 
         i += 1
-        if i == 29:  # only use 29 objects to make results comparable with lie-simulation
-            break
+        # if i == 29:  # only use 29 objects to make results comparable with lie-simulation
+        #     break
+sim.move_to_com()
 
 print(sim.N)
+assert sim.dt < innermost_period(sim) / 20
+# exit()
 # from matplotlib import pyplot as plt
 # OrbitPlot(sim,slices=1,color=True)
 # plt.show()
 
 max_n = sim.N
 print("start")
-tmax = 1e8
-savesteps = 20000
+tmax = 300
+savesteps = 300
 times = np.linspace(0., tmax, savesteps)
-sim.move_to_com()
-# sim.exit_max_distance = 15
+extradata.energy.set_initial_energy(sim.calculate_energy())
 
 extradata.meta.tmax = tmax
 extradata.meta.savesteps = savesteps
@@ -83,12 +105,15 @@ except OSError:
 # sim.automateSimulationArchive("water.bin", interval=1, deletefile=True)
 for i, t in enumerate(times, start=1):
     try:
-        sim.integrate(t)
-        print(sim.dt)
+        sim.integrate(t, exact_finish_time=0)
+        print("dt", sim.dt)
         merc: reb_simulation_integrator_mercurius = sim.ri_mercurius
-        print(t)
+        print("t", t)
+        print(merc.mode)
     except Collision:
         merge_particles(sim, extradata)
+    except Escape:
+        handle_escape(sim,extradata)
     except NoParticles:
         print("No Particles left")
         abort = True
@@ -100,7 +125,15 @@ for i, t in enumerate(times, start=1):
     extradata.meta.cputime = time.process_time()
     extradata.meta.current_time = t
     extradata.meta.current_steps = i
+    extradata.energy.add_energy_value(sim.calculate_energy())
+    total = 0
+    p: Particle
+    for p in sim.particles:
+        total += linalg.norm(p.vxyz) * p.m
+    print("total", total)
     extradata.save(fn.with_suffix(".extra.json"))
+    # assert sim.dt < innermost_period(sim) / 20
+    print("fraction", innermost_period(sim) / 20)
 
     if abort:
         exit(1)
