5b62945516116d877d3bc19ce21b28f45f59fd6f
Lukas Winkler <git@lw1.at>
2021-01-04 14:48:48 +0100

    simplify analyze.py

--- analyze.py
+++ analyze.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from rebound import SimulationArchive, Particle, Simulation
 
 from extradata import ExtraData
@@ -9,25 +8,10 @@ fn = filename_from_argv()
 sa = SimulationArchive(str(fn.with_suffix(".bin")))
 ed = ExtraData.load(fn.with_suffix(".extra.json"))
 print(ed.meta)
-# times = np.linspace(0., ed.meta.current_time, ed.meta.current_steps)
 
-# data = {}
-# for i, t in enumerate(times):
-#     try:
-#         sim = sa.getSimulation(t=t)
-#     except ValueError:
-#         print("save is incomplete")
-#         continue
-#         # break
-#     for pn in range(1, sim.N):
-#         part: Particle = sim.particles[pn]
-#         hash = part.hash.value
-#         if hash not in data:
-#             data[hash] = ([], [])
-#         data[hash][0].append(t)
-#         data[hash][1].append(part.a)
 data = {}
 sim: Simulation
+print(f"{len(sa)} Snapshots found")
 for sim in sa:
     t = sim.t
     for pn in range(1, sim.N):
