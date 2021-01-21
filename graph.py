30d57a3ea273329fac84cd914e67449d91563d7a
Lukas Winkler <git@lw1.at>
2021-01-21 18:50:44 +0100

    nicer graphs

--- graph.py
+++ graph.py
@@ -7,6 +7,8 @@ fn = filename_from_argv()
 ed = ExtraData.load(fn.with_suffix(".extra.json"))
 
 dot = Digraph(comment='Collisions')
+
+interacting_objects = set()
 for merged, originals in ed.tree._tree.items():
     first_parent = True
     for parent in originals["parents"]:
@@ -20,6 +22,17 @@ for merged, originals in ed.tree._tree.items():
         else:
             label = None
         dot.edge(str(parent), str(merged), xlabel=label)
+        interacting_objects.add(parent)
+        interacting_objects.add(int(merged))
+
+for name in interacting_objects:
+    object = ed.pdata[name]
+    if object.type == "sun":
+        name = "Sun"
+    if object.type == "gas giant":
+        name = "gas giant"
+    dot.node(name=str(name), label=f"{name} ({object.water_mass_fraction:.2e})",
+             shape='box' if object.type == "planetesimal" else "ellipse")
 
 # dot.engine = 'neato'
 dot.render(fn.with_suffix(".gv"), view=True, format="svg")
