e80c6fd36d10a76a134ec113728da314a27c944b
Lukas Winkler <git@lw1.at>
2021-03-03 14:59:14 +0100

    improve collision history plots

--- collisionhistory.py
+++ collisionhistory.py
@@ -1,4 +1,6 @@
 from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 from rebound import SimulationArchive, Simulation
 
 from extradata import ExtraData, CollisionMeta
@@ -11,6 +13,11 @@ sa = SimulationArchive(str(fn.with_suffix(".bin")))
 last_sim: Simulation = sa[-1]
 print([p.hash.value for p in last_sim.particles])
 print(last_sim.t)
+
+fig: Figure = plt.figure()
+ax_masses: Axes = fig.add_subplot(2,1, 1)
+ax_wmfs: Axes = fig.add_subplot(2,1, 2)
+
 for particle in last_sim.particles:
     if ed.pd(particle).type in ["sun", "gas giant"]:
         continue
@@ -44,8 +51,15 @@ for particle in last_sim.particles:
         continue
     masses = [p.total_mass for p in objects]
     wmfs = [p.water_mass_fraction for p in objects]
-    plt.step(times, wmfs, label=particle.hash.value)
-plt.xscale("log")
-plt.yscale("log")
-plt.legend()
+    figs = []
+    ax_masses.step(times, masses, label=particle.hash.value)
+    ax_wmfs.step(times, wmfs, label=particle.hash.value)
+    ax_wmfs.set_ylabel("water mass fraction")
+    ax_masses.set_ylabel("masses [kg]")
+    for ax in [ax_wmfs, ax_masses]:
+        ax.set_xlabel("time [yr]")
+        ax.set_xscale("log")
+        ax.set_yscale("log")
+        ax.legend()
+fig.tight_layout()
 plt.show()
